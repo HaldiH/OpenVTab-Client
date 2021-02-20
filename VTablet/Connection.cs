f8130e79e655fc40632d2bee4b0a12bf2c12b661
Hugo Haldi <20846785+ShinoYasx@users.noreply.github.com>
2021-02-20 22:35:15 +0100

    First code commit

---
+++ VTablet/Connection.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Windows.ApplicationModel.Core;
+using Windows.Devices.Bluetooth.Background;
+using Windows.Foundation;
+using Windows.UI.Input;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Microsoft.Win32.SafeHandles;
+
+namespace OpenVTab
+{
+    public enum PointerEventType
+    {
+        PointerCanceled = 0,
+        PointerCaptureLost = 1,
+        PointerEntered = 2,
+        PointerExited = 3,
+        PointerMoved = 4,
+        PointerPressed = 5,
+        PointerReleased = 6,
+        PointerWheelChanged = 7
+    }
+
+    public enum WindowEventType
+    {
+        WindowSizeChanged = 0
+    }
+
+    public enum EventType
+    {
+        Pointer = 0,
+        Window = 1
+    }
+
+    [Flags]
+    public enum PointerPropertiesFlags
+    {
+        None = 0,
+        TouchConfidence = 1 << 0,
+        LeftButtonPressed = 1 << 1,
+        RightButtonPressed = 1 << 2,
+        MiddleButtonPressed = 1 << 3,
+        Inverted = 1 << 4,
+        Eraser = 1 << 5,
+        HorizontalMouseWheel = 1 << 6,
+        Primary = 1 << 7,
+        InRange = 1 << 8,
+        Canceled = 1 << 9,
+        BarrelButtonPressed = 1 << 10,
+        XButton1Pressed = 1 << 11,
+        XButton2Pressed = 1 << 12
+    }
+
+    internal struct ConfigData
+    {
+        public double Width, Height;
+
+        public ConfigData(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+
+    internal class Connection
+    {
+        private readonly Socket _socket;
+        public Size CanvasSize { get; set; }
+
+        public Connection(string hostUri, short port)
+        {
+            _socket = GetHostSocket(hostUri, port);
+
+            if (IsConnected()) return;
+            Debug.WriteLine("Cannot connect to socket");
+        }
+
+
+        public bool IsConnected()
+        {
+            return _socket.Connected;
+        }
+
+        public int SendConfig(ConfigData data)
+        {
+            int rc;
+            var total = 0;
+            if ((rc = _socket.Send(BitConverter.GetBytes(data.Width))) < 0)
+                return -1;
+            total += rc;
+            if ((rc = _socket.Send(BitConverter.GetBytes(data.Height))) < 0)
+                return -1;
+            total += rc;
+            return total;
+        }
+
+        private int SendPointerPointProperties(PointerPointProperties properties)
+        {
+            var flags = PointerPropertiesFlags.None;
+            if (properties.TouchConfidence)
+                flags |= PointerPropertiesFlags.TouchConfidence;
+            if (properties.IsLeftButtonPressed)
+                flags |= PointerPropertiesFlags.LeftButtonPressed;
+            if (properties.IsRightButtonPressed)
+                flags |= PointerPropertiesFlags.RightButtonPressed;
+            if (properties.IsMiddleButtonPressed)
+                flags |= PointerPropertiesFlags.MiddleButtonPressed;
+            if (properties.IsInverted)
+                flags |= PointerPropertiesFlags.Inverted;
+            if (properties.IsEraser)
+                flags |= PointerPropertiesFlags.Eraser;
+            if (properties.IsHorizontalMouseWheel)
+                flags |= PointerPropertiesFlags.HorizontalMouseWheel;
+            if (properties.IsPrimary)
+                flags |= PointerPropertiesFlags.Primary;
+            if (properties.IsInRange)
+                flags |= PointerPropertiesFlags.InRange;
+            if (properties.IsCanceled)
+                flags |= PointerPropertiesFlags.Canceled;
+            if (properties.IsBarrelButtonPressed)
+                flags |= PointerPropertiesFlags.BarrelButtonPressed;
+            if (properties.IsXButton1Pressed)
+                flags |= PointerPropertiesFlags.XButton1Pressed;
+            if (properties.IsXButton2Pressed)
+                flags |= PointerPropertiesFlags.XButton2Pressed;
+
+            int rc;
+            var total = 0;
+            if ((rc = _socket.Send(BitConverter.GetBytes(properties.Pressure))) < 0)
+                return -1;
+            total += rc;
+            if ((rc = _socket.Send(BitConverter.GetBytes(properties.Orientation))) < 0)
+                return -1;
+            total += rc;
+            if ((rc = _socket.Send(BitConverter.GetBytes(properties.XTilt))) < 0)
+                return -1;
+            total += rc;
+            if ((rc = _socket.Send(BitConverter.GetBytes(properties.YTilt))) < 0)
+                return -1;
+            total += rc;
+            if ((rc = _socket.Send(BitConverter.GetBytes(properties.Twist))) < 0)
+                return -1;
+            total += rc;
+            if ((rc = _socket.Send(BitConverter.GetBytes(properties.MouseWheelDelta))) < 0)
+                return -1;
+            total += rc;
+            if ((rc = _socket.Send(BitConverter.GetBytes((UInt16) flags))) < 0)
+                return -1;
+            total += rc;
+            return total;
+        }
+
+        private int SendPoint(Point point)
+        {
+            int rc;
+            var total = 0;
+            if ((rc = _socket.Send(BitConverter.GetBytes(point.X))) < 0)
+                return -1;
+            total += rc;
+            if ((rc = _socket.Send(BitConverter.GetBytes(point.Y))) < 0)
+                return -1;
+            total += rc;
+            return total;
+        }
+
+        private int SendPointerPoint(PointerPoint ptrPt)
+        {
+            int rc;
+            var total = 0;
+            if ((rc = SendPoint(new Point(ptrPt.Position.X / CanvasSize.Width * 100,
+                ptrPt.Position.Y / CanvasSize.Height * 100))) < 0)
+                return -1;
+            total += rc;
+            if ((rc = SendPointerPointProperties(ptrPt.Properties)) < 0)
+                return -1;
+            total += rc;
+            return total;
+        }
+
+        private int SendPointerEventType(PointerEventType eventType)
+        {
+            return _socket.Send(new byte[] {(byte) eventType});
+        }
+
+        private int SendWindowEventType(WindowEventType eventType)
+        {
+            return _socket.Send(new byte[] {(byte) eventType});
+        }
+
+        private int SendEventType(EventType eventType)
+        {
+            return _socket.Send(new byte[] {(byte) eventType});
+        }
+
+        private int SendPointerEvent(PointerEventType eventType, PointerPoint ptrPt)
+        {
+            int rc;
+            var total = 0;
+            if ((rc = SendPointerEventType(eventType)) < 0)
+                return -1;
+            total += rc;
+            if ((rc = SendPointerPoint(ptrPt)) < 0)
+                return -1;
+            total += rc;
+            return total;
+        }
+
+        private int SendSize(Size size)
+        {
+            int rc;
+            var total = 0;
+            if ((rc = _socket.Send(BitConverter.GetBytes(size.Width))) < 0)
+                return -1;
+            total += rc;
+            if ((rc = _socket.Send(BitConverter.GetBytes(size.Height))) < 0)
+                return -1;
+            total += rc;
+            return total;
+        }
+
+        private int SendWindow(Window window)
+        {
+            return SendSize(window.Content.RenderSize);
+        }
+
+
+        private int SendWindowEvent(WindowEventType eventType, Window window)
+        {
+            int rc;
+            var total = 0;
+            if ((rc = SendWindowEventType(eventType)) < 0)
+                return -1;
+            total += rc;
+            if ((rc = SendWindow(window)) < 0)
+                return -1;
+            total += rc;
+            return total;
+        }
+
+        public int SendEvent(WindowEventType eventType, Window window)
+        {
+            int rc;
+            var total = 0;
+            if ((rc = SendEventType(EventType.Window)) < 0)
+                return -1;
+            total += rc;
+            if ((rc = SendWindowEvent(eventType, window)) < 0)
+                return -1;
+            total += rc;
+            return total;
+        }
+
+        public int SendEvent(PointerEventType eventType, PointerPoint ptrPt)
+        {
+            int rc;
+            var total = 0;
+            if ((rc = SendEventType(EventType.Pointer)) < 0)
+                return -1;
+            total += rc;
+            if ((rc = SendPointerEvent(eventType, ptrPt)) < 0)
+                return -1;
+            total += rc;
+            return total;
+        }
+
+        private static Socket GetHostSocket(string hostUri, short port)
+        {
+            var s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            if (IPAddress.TryParse(hostUri, out var ip))
+            {
+                s.Connect(ip, port);
+                return s;
+            }
+
+            foreach (var address in Dns.GetHostAddresses(hostUri))
+            {
+                if (address.AddressFamily != AddressFamily.InterNetwork)
+                    continue;
+                s.Connect(address, port);
+                if (s.Connected)
+                    break;
+            }
+
+            return s;
+        }
+    }
+}
---
+++ VTablet/MainPage.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Devices.Input;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Net.Sockets;
+using System.Text;
+using Windows.ApplicationModel.Core;
+
+namespace OpenVTab
+{
+    public sealed partial class MainPage : Page
+    {
+        private readonly Connection _conn;
+        private readonly Frame _frame;
+
+        public MainPage()
+        {
+            _frame = (Frame) Window.Current.Content;
+            _conn = new Connection("127.0.0.1", 4444);
+            this.InitializeComponent();
+
+            _frame.SizeChanged += new SizeChangedEventHandler(Container_SizeChanged);
+
+            // Declare the pointer event handlers.
+            Target.PointerCanceled += new PointerEventHandler(Target_PointerCanceled);
+            Target.PointerCaptureLost += new PointerEventHandler(Target_PointerCaptureLost);
+            Target.PointerEntered += new PointerEventHandler(Target_PointerEntered);
+            Target.PointerExited += new PointerEventHandler(Target_PointerExited);
+            Target.PointerMoved += new PointerEventHandler(Target_PointerMoved);
+            Target.PointerPressed += new PointerEventHandler(Target_PointerPressed);
+            Target.PointerReleased += new PointerEventHandler(Target_PointerReleased);
+            Target.PointerWheelChanged += new PointerEventHandler(Target_PointerWheelChanged);
+
+            //_conn.SendConfig(new ConfigData(Target.Width, Target.Height));
+            _conn.CanvasSize = Target.RenderSize;
+        }
+
+        private void Container_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var width = _frame.ActualWidth;
+            var height = _frame.ActualHeight;
+            Target.Width = width;
+            Target.Height = height;
+            Container.Width = width;
+            Container.Height = height;
+            _conn.CanvasSize = new Size(width, height);
+            //_conn.SendEvent(WindowEventType.WindowSizeChanged, Window.Current);
+        }
+
+
+        private void Target_PointerCanceled(object sender, PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+            _conn.SendEvent(PointerEventType.PointerCanceled, e.GetCurrentPoint(Target));
+        }
+
+        private void Target_PointerCaptureLost(object sender, PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+            _conn.SendEvent(PointerEventType.PointerCaptureLost, e.GetCurrentPoint(Target));
+        }
+
+        private void Target_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+            _conn.SendEvent(PointerEventType.PointerEntered, e.GetCurrentPoint(Target));
+        }
+
+        private void Target_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+            _conn.SendEvent(PointerEventType.PointerExited, e.GetCurrentPoint(Target));
+        }
+
+        private void Target_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+            _conn.SendEvent(PointerEventType.PointerMoved, e.GetCurrentPoint(Target));
+        }
+
+        private void Target_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+            _conn.SendEvent(PointerEventType.PointerPressed, e.GetCurrentPoint(Target));
+        }
+
+        private void Target_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+            _conn.SendEvent(PointerEventType.PointerReleased, e.GetCurrentPoint(Target));
+        }
+
+        private void Target_PointerWheelChanged(object sender, PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+            _conn.SendEvent(PointerEventType.PointerWheelChanged, e.GetCurrentPoint(Target));
+        }
+    }
+}
